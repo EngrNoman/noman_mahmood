21d91358cf312e333382b6cfb373f740450d7413
Noman Mahmood <nomanmahmood730@gmail.com>
2025-09-13 10:16:50 +0500

    Trigger redeploy with new commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/noman_mahmood/", // yahan repo name likho
-});
+  base: '/noman_mahmood/', // 👈 repo ka naam yahan likho
+})
